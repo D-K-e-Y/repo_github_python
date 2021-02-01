c1bab8d9368177392696df97cf866767eac53972
D-K-e-Y <kushnarev_den@hotmail.com>
2021-02-01 23:21:21 +0100

    Test_zone

--- Hmw_4/hmw_4_4.py
+++ Hmw_4/hmw_4_4.py
@@ -2,4 +2,3 @@ my_list = [7, 8, 8, 8, 9, 12, 11, 72, 35, 11, 11, 17, 8, 26]
 my_new_list = [el for el in my_list if my_list.count(el) < 2]
 print(my_list)
 print(my_new_list)
-#test
---
+++ Hmw_5/hmw_5_1.py
@@ -0,0 +1,11 @@
+f = open('data.txt', 'tw+')
+data = input('Введите данные: ')
+while data:
+    f.writelines(data)
+    data = input('Введите данные: ')
+    if not data:
+        break
+result = f.readlines()
+print(result)
+f.close()
+#test
