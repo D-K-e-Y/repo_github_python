f74d5b8f7171853638cc5bc909c6730d460d93f4
D-K-e-Y <kushnarev_den@hotmail.com>
2021-02-05 21:56:06 +0100

    Hmw_6

---
+++ pythonProject8/hmw_6_1.py
@@ -0,0 +1,8 @@
+class Trafficlight:
+    color_red = red
+    color_yellow = yellow
+    color_green = green
+    count = 0
+    def __color_trafic(self):
+        return f"{self.color}"
+    Trafficlight.count += 1
